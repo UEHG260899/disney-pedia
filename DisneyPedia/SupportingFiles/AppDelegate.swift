e330681b4d45485625b00b1a965763a9f7d1bffa
Uriel Enrique <urielenrique99@gmail.com>
2022-08-04 21:45:46 -0500

    deleting unnecesary files

---
+++ DisneyPedia/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DisneyPedia
+//
+//  Created by Uriel Hernandez Gonzalez on 04/08/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
