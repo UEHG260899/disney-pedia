23bd6ba4bc3ddf2b789ec16dd6c5b885780ca155
Uriel Enrique <urielenrique99@gmail.com>
2022-08-04 21:35:07 -0500

    Initial Commit

---
+++ DisneyPedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisneyPedia
+//
+//  Created by Uriel Hernandez Gonzalez on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
