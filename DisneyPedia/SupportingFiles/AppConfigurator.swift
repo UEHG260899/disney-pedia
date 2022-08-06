6cfeca185d546b3969dd3f5871502baa2564f89a
Uriel Enrique <urielenrique99@gmail.com>
2022-08-06 18:41:43 -0500

    configuring navBar text styles

--- DisneyPedia/SupportingFiles/AppConfigurator.swift
+++ DisneyPedia/SupportingFiles/AppConfigurator.swift
@@ -9,6 +9,13 @@ import UIKit
 
 struct AppConfigurator {
     
+    enum TextStyles {
+        static let titleTextStyles: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "LobsterTwo-Bold", size: 17.0),
+            .foregroundColor: UIColor.black
+        ]
+    }
+    
     
     static func configureNavigationController() -> UINavigationController {
         let navController = UINavigationController()
@@ -17,6 +24,7 @@ struct AppConfigurator {
         navController.navigationBar.prefersLargeTitles = false
         navController.navigationBar.barStyle = .black
         navController.navigationBar.shadowImage = UIImage()
+        navController.navigationBar.titleTextAttributes = TextStyles.titleTextStyles
         return navController
     }
 }
